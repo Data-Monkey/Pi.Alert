9e2cc8a5b0c8adc5d0a5dc0c79ff1a1411b2fd53
Data-Monkey <roland.beck@dulli.ch>
2023-06-18 16:31:35 +1000

    pylint ntfy

--- pialert/publishers/ntfy.py
+++ pialert/publishers/ntfy.py
@@ -1,21 +1,27 @@
+"""
+ntfy platform integration
+"""
+from base64 import b64encode
+import requests
 
 import conf
-import requests
-from base64 import b64encode
 
 from logger import mylog
 from helper import noti_struc
 
 #-------------------------------------------------------------------------------
 def check_config():
+    """ check all the required configuration parameters are set """
     if conf.NTFY_HOST == '' or conf.NTFY_TOPIC == '':
+        # pylint: disable-next=line-too-long
         mylog('none', ['[Check Config] Error: NTFY service not set up correctly. Check your pialert.conf NTFY_* variables.'])
         return False
     else:
         return True
-    
+
 #-------------------------------------------------------------------------------
 def send  (msg: noti_struc):
+    """ sending the message to ntfy """
 
     headers = {
         "Title": "Pi.Alert Notification",
@@ -27,17 +33,19 @@ def send  (msg: noti_struc):
     if conf.NTFY_USER != "" and conf.NTFY_PASSWORD != "":
 	# Generate hash for basic auth
         # usernamepassword = "{}:{}".format(conf.NTFY_USER,conf.NTFY_PASSWORD)
-        basichash = b64encode(bytes(conf.NTFY_USER + ':' + conf.NTFY_PASSWORD, "utf-8")).decode("ascii")
+        basichash = b64encode(bytes(conf.NTFY_USER + ':' +
+                                    conf.NTFY_PASSWORD, "utf-8")).decode("ascii")
 
 	# add authorization header with hash
-        headers["Authorization"] = "Basic {}".format(basichash)
+        headers["Authorization"] = f'Basic {basichash}'
 
     try:
-        requests.post("{}/{}".format( conf.NTFY_HOST, conf.NTFY_TOPIC),
-                                        data=msg.text,
-                                        headers=headers)
-    except requests.exceptions.RequestException as e:  
-        mylog('none', ['[NTFY] Error: ', e])
+        requests.post(url=f'{conf.NTFY_HOST}/{conf.NTFY_TOPIC}',
+                      data=msg.text,
+                      headers=headers,
+                      timeout=10)
+    except requests.exceptions.RequestException as exception:
+        mylog('none', ['[NTFY] Error: ', exception])
         return -1
 
-    return 0    
+    return 0
